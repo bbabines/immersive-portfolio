ed545c63d5f7d152f5125c30508942d64de59b28
Brad Babines <bbabines@gmail.com>
2023-08-22 14:22:49 -0400

    feat: removed links on component card

--- src/app/selection-screen/page.tsx
+++ src/app/selection-screen/page.tsx
@@ -11,8 +11,8 @@ const SelectionScreen = () => {
 				{/* Classic Website */}
 				<div className="py-4 flex flex-col justify-center items-center">
 					<h3 className="py-4 text-[2rem] text-[#eab832]">Classic Website</h3>
-					<div className="flex flex-wrap justify-center items-center gap-4">
-						<div className="w-[300px] p-4 mb-4 bg-[#151030] rounded-2xl">
+					<div className="flex flex-wrap justify-center items-center gap-4 ">
+						<div className="w-[300px] p-4 mb-4 bg-[#151030] rounded-2xl ">
 							<div>
 								<div className="flex items-center justify-center max-h-[150px]">
 									<img
@@ -21,51 +21,13 @@ const SelectionScreen = () => {
 										className=" h-[150px] object-contain rounded-2xl"
 									/>
 								</div>
-								<p className="my-2 font-bold">Classic Website Experience</p>
 								<p className="font-light text-[0.95rem] text-white">
-									This will give you a classic website experience with some 3D
-									components
+									This is a classic website experience with some 3D components.
 								</p>
 								<div className="my-2 flex justify-center">
-									<p className="mx-2 blue-text-gradient">
-										{/* {project.tags[0].name} */}
-										List 1
-									</p>
-									<p className="mx-2 green-text-gradient">
-										{/* {project.tags[1].name} */}
-										List 1
-									</p>
-									<p className="mx-2 pink-text-gradient">
-										{/* {project.tags[2].name} */}
-										List 1
-									</p>
-								</div>
-								<div className="flex justify-between">
-									<a
-										// href={project.source_code_link}
-
-										target="_blank"
-									>
-										<div className="h-[40px] w-[40px] black-gradient rounded-full flex justify-center items-center border border-[#eab832] hover:border-[#86efac] cursor-pointer">
-											<img
-												src="github.png"
-												alt="GitHub icon"
-												className="h-[90%] w-[90%] object-contain"
-											/>
-										</div>
-									</a>
-									<a
-										// href={project.live_demo_link}
-										target="_blank"
-									>
-										<div className="h-[40px] w-[40px] black-gradient rounded-full flex justify-center items-center border border-[#eab832] hover:border-[#86efac] cursor-pointer">
-											<img
-												src="demo.png"
-												alt="Demo icon"
-												className="h-[65%] object-contain"
-											/>
-										</div>
-									</a>
+									<p className="mx-2 blue-text-gradient">Next.js</p>
+									<p className="mx-2 green-text-gradient">Typescript</p>
+									<p className="mx-2 pink-text-gradient">React</p>
 								</div>
 							</div>
 						</div>
@@ -87,51 +49,14 @@ const SelectionScreen = () => {
 										className=" h-[150px] object-contain rounded-2xl"
 									/>
 								</div>
-								<p className="my-2 font-bold">Classic Website Experience</p>
 								<p className="font-light text-[0.95rem] text-white">
-									This will give you a classic website experience with some 3D
-									components
+									This is a fully immersive experience where you control an
+									avatar and interaction with the environment.
 								</p>
 								<div className="my-2 flex justify-center">
-									<p className="mx-2 blue-text-gradient">
-										{/* {project.tags[0].name} */}
-										List 1
-									</p>
-									<p className="mx-2 green-text-gradient">
-										{/* {project.tags[1].name} */}
-										List 1
-									</p>
-									<p className="mx-2 pink-text-gradient">
-										{/* {project.tags[2].name} */}
-										List 1
-									</p>
-								</div>
-								<div className="flex justify-between">
-									<a
-										// href={project.source_code_link}
-
-										target="_blank"
-									>
-										<div className="h-[40px] w-[40px] black-gradient rounded-full flex justify-center items-center border border-[#eab832] hover:border-[#86efac] cursor-pointer">
-											<img
-												src="github.png"
-												alt="GitHub icon"
-												className="h-[90%] w-[90%] object-contain"
-											/>
-										</div>
-									</a>
-									<a
-										// href={project.live_demo_link}
-										target="_blank"
-									>
-										<div className="h-[40px] w-[40px] black-gradient rounded-full flex justify-center items-center border border-[#eab832] hover:border-[#86efac] cursor-pointer">
-											<img
-												src="demo.png"
-												alt="Demo icon"
-												className="h-[65%] object-contain"
-											/>
-										</div>
-									</a>
+									<p className="mx-2 blue-text-gradient">Next.js</p>
+									<p className="mx-2 green-text-gradient">React Three Fiber</p>
+									<p className="mx-2 pink-text-gradient">React</p>
 								</div>
 							</div>
 						</div>
