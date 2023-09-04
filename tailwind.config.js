dbfdde1e3911c3d3b17518eff5f3545c4a4f9b67
Brad Babines <bbabines@gmail.com>
2023-09-04 13:06:41 -0400

    feat: added custom glow to tailwind and removed linting rules for next.js build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			boxShadow: {
+				leftGlow: "-4px 4px 14px 0 rgba(234, 184, 26, 0.69)", // Adjusted X and Y to 4px each.
+				rightGlow: "4px 4px 14px 0 rgba(234, 184, 26, 0.69)", // Adjusted X and Y to 4px each.
+			},
+		},
+	},
+	plugins: [],
+};
